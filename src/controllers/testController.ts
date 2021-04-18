00a7fb1e22a2ada894c5f3e0404cbbda459f56a4
MacBook <macbook@MacBook-Pro-MacBook.local>
2021-04-18 20:01:05 +0300

    change auth controller

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -1,28 +1,73 @@
 import { Request, Response } from 'express'
-import { User } from 'src/entities/User'
-
 
 export const reg = async (req: Request, res: Response) => {
 
   try { 
    const {  username, email, password } = req.body
+//    const salt = await bcrypt.genSalt(10 )
+//    const hashPassword = await bcrypt.hash(password, salt)
 
-
-   const user = await User.createQueryBuilder()   
-      .insert()
-      .values({
-           username,
-           email, 
-           password
-      })
-      .returning('*')
-      .execute()
-
+//    const user = await User.createQueryBuilder()   
+//       .insert()
+//       .values({
+//            username,
+//            email, 
+//            password: hashPassword 
+//       })
+//       .returning('*')
+//       .execute()
+//   //   getConnection()
+//   //  .createQueryBuilder()
+//   //  .insert()
+//   //  .into(User)
+//   //  .values(
+//   //      , 
+//   //   )
+//   //  .returning("*")
+//   //  .execute() 
+//  console.log('User', user)
+//    const payload = { id: user.raw[0].id }
+//    const token = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET as string)
    res.send({ 
-    data: user.raw,
+     data: {
+         username,
+         email,
+         password
+     }  
   });
 
   } catch (err) {
     console.log("Error", err);
   }
 }
+
+// export const log = async (req: Request, res: Response) => {
+//   try {
+//     const { email, password } = req.body
+//     const user =  await User.createQueryBuilder()        
+//         .where("user.email = :email", { email })
+//         .getOne()
+       
+//         if(user) {
+//           const validPassword = await bcrypt.compare(password, user.password)
+//           if (!validPassword) {
+//              res.status(401).send({
+//             error: {
+//               message: "Password is wrong"
+//               }               
+//             })
+//           }
+//             //const token = jwt.sign({ id: user.id }, process.env.ACCESS_TOKEN_SECRET as string)
+
+//             res.send({ 
+//               data: user,
+//               "token": token 
+//             })
+          
+//         }
+
+//   } catch (err) {
+//     console.log("Error", err);
+//   }
+// };
+
