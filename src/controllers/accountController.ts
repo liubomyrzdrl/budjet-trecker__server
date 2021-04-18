94ded86d6f491de9f73a4ee25a09ab26b3d5f05b
MacBook <macbook@MacBook-Pro-MacBook.local>
2021-04-19 00:55:36 +0300

    env problem fix

--- src/controllers/accountController.ts
+++ src/controllers/accountController.ts
@@ -3,14 +3,21 @@ import { getConnection } from "typeorm"
 import { User } from '../entities/User'
 
 export const account = async ( req: any, res: Response) => {
+
     try {
+        // const user = await User.createQueryBuilder()
+        // .where("user.id = :id", { id })
+        // .getOne()   
+
+        // res.status(200).send({ 
+        //     data: user          
+        // })
         const user = await getConnection()
         .getRepository(User)
         .createQueryBuilder("user")
         .where("user.id = :id", { id: Number(req.user.id)})
-        .getOne()  
-
-        res.send({ 
+        .getOne()      
+        res.status(200).send({ 
             data: user          
         })
     } catch (error) {
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -53,7 +53,7 @@ export const login = async (req: Request, res: Response) => {
 
             res.send({ 
               data: user,
-              "token": token 
+              token 
             })
           
         }
