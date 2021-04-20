7b5e837c26d35f07670ebc8b7f2825994a23da85
MacBook <macbook@MacBook-Pro-MacBook.local>
2021-04-20 11:51:59 +0300

    change entries create

--- src/controllers/entriesController.ts
+++ src/controllers/entriesController.ts
@@ -7,7 +7,7 @@ export const get = async ( req: Request, res: Response) => {
       const { userId, date } = req.query
     
       const entries = await Entrie.query(`
-           SELECT * FROM entrie e WHERE e."userId"= $1 and e.date= $2
+          SELECT * FROM entrie e WHERE e."userId"= $1 and e.date= $2
       `,[userId, date])
       res.send(entries)
     } catch (error) {
@@ -18,7 +18,7 @@ export const get = async ( req: Request, res: Response) => {
 export const create = async ( req: Request, res: Response) => {
     try {
       const { catagorie, amount, userId, date } = req.body
-        const createdUser = await Entrie.create({
+      const createdUser = await Entrie.create({
           catagorie,
           amount,
           userId,
