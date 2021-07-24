24af7816e4a5c35d54286209a6eb6892fadc5d24
MacBook <macbook@MacBook-Pro-MacBook.local>
2021-07-24 12:09:19 +0300

    change server

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -6,11 +6,11 @@ import { ConnectionOptions } from 'typeorm'
 
 export default {
     type: 'postgres',
-    "database": "dcuva41horg1mv",
-    "host": "ec2-52-50-171-4.eu-west-1.compute.amazonaws.com",
+    "database": "de9spueqlas61b",
+    "host": "ec2-54-228-174-49.eu-west-1.compute.amazonaws.com",
     "port": 5432,
-    "username": "qzdaoiomewrxjn",
-    "password": "13172cbcfd564553a3d2ab072702f2e3e047e81de3fb7729c2433cb75475d856",
+    "username": "rcgqcnqqybbzxj",
+    "password": "f8a8e88ff4e1909aac2562e530f7ffa5357092896b2dede157633136f59edc5d",
     "synchronize": true,
     "entities": [
 
