ab23e45ca537cb5744ea18040b423d80d17e4e76
MacBook <macbook@MacBook-Pro-MacBook.local>
2021-10-06 21:46:57 +0300

    add anjther server settings

--- src/index.ts
+++ src/index.ts
@@ -11,16 +11,16 @@ const main = async () => {
   const app = express()
   await createConnection({
   type: "postgres",
-  host: "ec2-52-86-2-228.compute-1.amazonaws.com",
+  host: "ec2-52-209-171-51.eu-west-1.compute.amazonaws.com",
   port: 5432,
-  username: "cggpbentastmeh",
-  password: "ffed1767d46f64fd9f7b0371bed296c1079ffb17cc523065a47f66be0d7888b5",
-  database: "d9mpkuvh5oa1b9",
+  username: "getmxiobnrejka",
+  password: "2eeff9562e26eaaf299dae756f0d973ac57eeec6595b553d6aead25354a113af",
+  database: "d20pf48roblj9t",
   ssl: { rejectUnauthorized: false },
   synchronize: true,
   logging: true,
   entities: ["dist/entities/*.{ts,js}"], 
-  url: "postgres://cggpbentastmeh:ffed1767d46f64fd9f7b0371bed296c1079ffb17cc523065a47f66be0d7888b5@ec2-52-86-2-228.compute-1.amazonaws.com:5432/d9mpkuvh5oa1b9"
+  url: "postgres://getmxiobnrejka:2eeff9562e26eaaf299dae756f0d973ac57eeec6595b553d6aead25354a113af@ec2-52-209-171-51.eu-west-1.compute.amazonaws.com:5432/d20pf48roblj9t"
 
  });
   app.use(cors({}))
